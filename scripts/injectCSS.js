2436e02facc7beb9f6da59ec9479c972d9769081
jetrotal <45118493+jetrotal@users.noreply.github.com>
2022-03-05 20:30:56 -0300

    Update injectCSS.js

--- scripts/injectCSS.js
+++ scripts/injectCSS.js
@@ -1,1 +1,16 @@
-
+function injectCSS(){
+document.body.innerHTML += `
+<style>
+#assetPointer{
+    vertical-align: middle;
+}
+.inner {
+    position: relative;
+    max-width: calc(100% - 100px) !important;
+}
+summary{
+cursor: pointer
+}
+</style>
+`
+}
