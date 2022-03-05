6a28418532467c492551ea5fbded1efd88ea481f
jetrotal <45118493+jetrotal@users.noreply.github.com>
2022-03-05 20:25:06 -0300

    Create injectCSS.js

---
+++ scripts/injectCSS.js
@@ -0,0 +1,14 @@
+document.body.innerHTML += `
+<style>
+#assetPointer{
+    vertical-align: middle;
+}
+.inner {
+    position: relative;
+    max-width: calc(100% - 100px) !important;
+}
+summary{
+cursor: pointer
+}
+</style>
+`
