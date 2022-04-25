faee2d1e1c47084980f9ae0ca342d221784d7ee1
jetrotal <45118493+jetrotal@users.noreply.github.com>
2022-04-25 18:36:27 -0300

    Update checklistGenerator.js

--- scripts/checklistGenerator.js
+++ scripts/checklistGenerator.js
@@ -241,7 +241,9 @@ function placeBar(obj) {
     obj = updateBar(obj)[0];
     document.getElementById("rtpBar").innerHTML = obj.html.join("");
     var currFolder = urlParams.get("filter") == gitData.assetsFolders ? "[ " + gitData.assetsFolders.join(" + ") + " ]" : "OpenRTP";
-    document.getElementById("rtpTotal").innerHTML = currFolder + " Collection is " + Math.round(obj.percent[0]) + "% Completed.<br> " + obj.total + " assets loaded.";
+    document.getElementById("rtpTotal").innerHTML = currFolder + " Collection is " + Math.round(obj.percent[0]) + "% Completed.<br> "+
+        `<p dir="auto">Send your Art: <a href="https://t.co/K4AXAeG0Yp" rel="nofollow"><code>EasyRPG Forum</code></a> <a href="https://github.com/EasyRPG/RTP/issues/25"><code>Guide for Artists</code></a><a href="https://t.co/0MJZlivP9D" rel="nofollow"> <code>Discord</code></a></p><br>` 
+        + obj.total + " assets loaded.";
 }
 
 start();
